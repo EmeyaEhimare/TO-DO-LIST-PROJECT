052c858102ba958d9423d2579c5012a8612bff4d
Emeya Ehimare <emeya.ehimare@gmail.com>
2024-09-04 11:45:26 +0100

    Update Homepage.js

--- client/src/scenes/Homepage.js
+++ client/src/scenes/Homepage.js
@@ -38,9 +38,7 @@ function HomePage() {
     <p>Simple to use; create, delete, edit your tasks and to-dos! Whether it be your school assignments or grocery shopping, we have you covered.</p>
     <a href='/register' className="button-37">Sign up</a> 
     </div>
-    <div className="footer">
-      <a className = "footerTag" href='https://github.com/jsan96/To-do-list-mern-app'> 🖥️ Find us on GitHub </a>
-    </div>
+   
   </div>
   );
 }
