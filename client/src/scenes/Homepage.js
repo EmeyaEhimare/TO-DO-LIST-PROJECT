fac128c184be37f04762c9d31cab059aafc999f4
Ehimare <victorymartial11@gmail.com>
2024-09-04 11:43:13 +0100

    changes

--- client/src/scenes/Homepage.js
+++ client/src/scenes/Homepage.js
@@ -1,13 +1,10 @@
-import React, {useEffect, useState} from 'react';
-
-
+import React, { useEffect, useState } from "react";
 
 function HomePage() {
-
-  const [backendData, setBackendData] = useState(null)
+  const [backendData, setBackendData] = useState(null);
 
   useEffect(() => {
-/*    fetch("http://localhost:8000").then(
+    /*    fetch("http://localhost:8000").then(
     response => response.json()
     ).then(
       data => {
@@ -18,30 +15,30 @@ function HomePage() {
     ) */
 
     const fetchData = async () => {
+      const response = await fetch(`http://localhost:8000`);
 
-      const response = await fetch(`http://localhost:8000`)
-   
-      const resData = await response.json()
-      setBackendData(resData.message)
-   
-     }
-     fetchData()
-}, [])
+      const resData = await response.json();
+      setBackendData(resData.message);
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="wrapper">
-    <div className="header">
-            <h1>{backendData}</h1>
+      <div className="header">
+        <h1>{backendData}</h1>
+      </div>
+      <div className="content">
+        <h1>Trouble staying organized? We gotchu.</h1>
+        <p>
+          Simple to use; create, delete, edit your tasks and to-dos! Whether it
+          be your school assignments or grocery shopping, we have you covered.
+        </p>
+        <a href="/register" className="button-37">
+          Sign up
+        </a>
+      </div>
     </div>
-    <div className="content">
-    <h1>Trouble staying organized? We gotchu.</h1> 
-    <p>Simple to use; create, delete, edit your tasks and to-dos! Whether it be your school assignments or grocery shopping, we have you covered.</p>
-    <a href='/register' className="button-37">Sign up</a> 
-    </div>
-    <div className="footer">
-      <a className = "footerTag" href='https://github.com/jsan96/To-do-list-mern-app'> 🖥️ Find us on GitHub </a>
-    </div>
-  </div>
   );
 }
 
